c222c176e9bf25ed5618e857e79c7a9aced69f5d
Yatish2012 <yatishgoyal27@gmail.com>
2025-09-03 00:06:53 +0530

    Update TUT2Q5.cpp

--- TUT2Q5.cpp
+++ TUT2Q5.cpp
@@ -1,134 +1,135 @@
-//5(a)
-#include <iostream>
-using namespace std;
+//(a) diagonal metrix
 
-class Diagonal {
-    int *A, n;
-public:
-    Diagonal(int n) { this->n=n; A=new int[n]; }
-    void set(int i,int j,int x){ if(i==j) A[i-1]=x; }
-    int get(int i,int j){ return (i==j)?A[i-1]:0; }
-    void display(){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                cout<<get(i,j)<<" ";
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter no. of non zero elements  : ";
+    cin>>n;
+    int arr[n] = {1,2,3,4};
+    int index=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                cout<<arr[index];
+                index++;
             }
-            cout<<endl;
+            else{
+                cout<<0;
+            }
+        }
+        cout<<endl;
+    }
+}
+
+//(b)->Tri-diagonal Matrix
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter size of array : ";
+    int n;
+    cin>>n;
+    int nonZero = 3*n- 2;
+    int arr[3*n-2];
+    for(int i=0;i<3*n-2;i++){ 
+        cin>>arr[i];
+    }
+    int index=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j || i=j+1 || i=j-1){
+                cout<<arr[index];
+                index++; 
+            }
+            else{
+                cout<<0;
+            }
+        }
+        cout<<endl;
+    }
+
+}
+
+//(c)->lower triangle matrix 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int nonZero = (n*(n+1))/2;
+    int arr[nonZero];
+    for(int i=0;i<nonZero;i++){ 
+        cin>>arr[i];
+    }
+    int index=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i<=j){
+                cout<<arr[index];
+                index++; 
+            }
+            else{
+                cout<<0;
+            }
+        }
+        cout<<endl;
+    }
+}
+
+//(d)-> Upper triangle matrix
+//(c)->lower triangle matrix 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int nonZero = (n*(n+1))/2;
+    int arr[nonZero];
+    for(int i=0;i<nonZero;i++){ 
+        cin>>arr[i];
+    }
+    int index=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i>=j){
+                cout<<arr[index];
+                index++; 
+            }
+            else{
+                cout<<0;
+            }
+        }
+        cout<<endl;
+    }
+}
+
+//(e)-> Symmetric Matrix
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int unique = (n*(n+1))/2;
+
+    int arr[unique];
+    cout<<"enter upper or lower triangle elements : ";
+    for(int i=0;i<unique;i++){
+        cin>>arr[i];
+    }
+    int index=0;
+    int crr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            crr[i][j]=arr[index];
+            crr[j][i]=arr[index];
+            index++;
         }
     }
-};
 
-int main(){
-    Diagonal d(3);
-    d.set(1,1,5); d.set(2,2,8); d.set(3,3,9);
-    d.display();
-    return 0;
-}
-
-
-//5(b)
-#include <iostream>
-using namespace std;
-
-class TriDiagonal {
-    int *A, n;
-public:
-    TriDiagonal(int n){ this->n=n; A=new int[3*n-2]; }
-    void set(int i,int j,int x){
-        if(i-j==0) A[n-1+i-1]=x;
-        else if(i-j==1) A[i-2]=x;
-        else if(j-i==1) A[2*n-1+i-1]=x;
-    }
-};
-
-int main(){
-    TriDiagonal t(4);
-    t.set(1,1,5);
-    t.set(2,1,2); t.set(2,2,6); t.set(2,3,7);
-    return 0;
-}
-
-//5(c)
-#include <iostream>
-using namespace std;
-
-class LowerTri {
-    int *A, n;
-public:
-    LowerTri(int n){ this->n=n; A=new int[n*(n+1)/2]; }
-    void set(int i,int j,int x){ if(i>=j) A[i*(i-1)/2+j-1]=x; }
-    void display(){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i>=j) cout<<A[i*(i-1)/2+j-1]<<" ";
-                else cout<<"0 ";
-            }
-            cout<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<crr[i][j];
         }
     }
-};
-
-int main(){
-    LowerTri l(3);
-    l.set(1,1,5); l.set(2,1,3); l.set(2,2,8);
-    l.set(3,1,4); l.set(3,2,6); l.set(3,3,9);
-    l.display();
-    return 0;
 }
 
-//5(d)
-#include <iostream>
-using namespace std;
-
-class UpperTri {
-    int *A, n;
-public:
-    UpperTri(int n){ this->n=n; A=new int[n*(n+1)/2]; }
-    void set(int i,int j,int x){ if(i<=j) A[j*(j-1)/2+i-1]=x; }
-    void display(){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i<=j) cout<<A[j*(j-1)/2+i-1]<<" ";
-                else cout<<"0 ";
-            }
-            cout<<endl;
-        }
-    }
-};
-
-int main(){
-    UpperTri u(3);
-    u.set(1,1,5); u.set(1,2,3); u.set(1,3,4);
-    u.set(2,2,8); u.set(2,3,6);
-    u.set(3,3,9);
-    u.display();
-    return 0;
-}
-
-//5(e)
-#include <iostream>
-using namespace std;
-
-class Symmetric {
-    int *A, n;
-public:
-    Symmetric(int n){ this->n=n; A=new int[n*(n+1)/2]; }
-    void set(int i,int j,int x){ if(i>=j) A[i*(i-1)/2+j-1]=x; }
-    int get(int i,int j){ return (i>=j)?A[i*(i-1)/2+j-1]:A[j*(j-1)/2+i-1]; }
-    void display(){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                cout<<get(i,j)<<" ";
-            }
-            cout<<endl;
-        }
-    }
-};
-
-int main(){
-    Symmetric s(3);
-    s.set(1,1,5); s.set(2,1,3); s.set(2,2,8);
-    s.set(3,1,4); s.set(3,2,6); s.set(3,3,9);
-    s.display();
-    return 0;
-}
