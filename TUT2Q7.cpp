ac3e035183d9e1965457bad5a106dbbfbbf90a78
Yatish2012 <yatishgoyal27@gmail.com>
2025-09-03 00:22:00 +0530

    Update TUT2Q7.cpp

--- TUT2Q7.cpp
+++ TUT2Q7.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int countInversions(int arr[], int n){
-    int inv=0;
+int main(){
+    int n;
+    cin>>n;
+    int arr[i];
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]>arr[j]) inv++;
+        cin>>arr[i];
+    }
+
+    int countInversions = 0;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i<j && arr[i]>arr[j]){
+                countInversions++;
+            } 
         }
     }
-    return inv;
-}
-
-int main(){
-    int arr[]={2,4,1,3,5};
-    int n=5;
-    cout<<"Number of inversions: "<<countInversions(arr,n);
-    return 0;
+    cout<<"no. of inversions = "<<countInversions;
 }
