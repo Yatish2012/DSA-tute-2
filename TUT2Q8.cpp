112b2fb07de71229a24e0bac9113f9eea5052e78
Yatish2012 <yatishgoyal27@gmail.com>
2025-09-03 00:19:18 +0530

    Update TUT2Q8.cpp\

--- TUT2Q8.cpp
+++ TUT2Q8.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
-#include <set>
+//8) Write a program to count the total number of distinct elements in an array of length n.
+#include<iostream>
 using namespace std;
-
 int main(){
-    int arr[]={1,2,3,2,4,1,5};
-    int n=7;
-    set<int> s(arr,arr+n);
-    cout<<"Total distinct elements: "<<s.size();
-    return 0;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    int i=0;
+    for(int j=1;j<n;j++){
+        if(arr[j]!=arr[i]){
+            
+            i++;
+        }
+    }
+    cout<<"unique elements = ";
+    cout<<i+1;
 }
